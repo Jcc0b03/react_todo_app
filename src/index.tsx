245bd50c86776c3f8e48dfd86a366752b329d7f2
jakub.wierzchowski <jakub.wierzchowski4@gmail.com>
2024-03-27 13:47:44 +0100

    created simple interface; TODO: form for adding TODOs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,103 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import DoneIcon from '-!svg-react-loader!./DoneIcon.svg';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import PlusIcon from '-!svg-react-loader!./PlusIcon.svg';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const TodayInfo = () => {
+
+    const days = ["Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Nie"]
+    const months = ["sty.", "lut.", "mar.", "kwi.", "maj", "cze.", "lip.", "sie.", "wrz.", "paź.", "lis.", "gru."];
+
+    const Today : Date = new Date();
+
+    const [unusualHoliday, setUnusualHoliday] = useState<string>("");
+
+    useEffect(() => {
+        const fetchUnusualHoliday = async () => {
+            const url = `https://pniedzwiedzinski.github.io/kalendarz-swiat-nietypowych/${Today.getMonth()+1}/${Today.getDate()}.json`;
+            try {
+                const data = await fetch(url);
+                if(!data.ok){
+                    throw new Error("error while fetching data")
+                }
+
+                const unusualHoliday = await data.json();
+                setUnusualHoliday(unusualHoliday[Math.floor(Math.random()*unusualHoliday.length)].name);
+            }catch(error){
+                console.log(error);
+            }
+        }
+
+        fetchUnusualHoliday();
+    }, [])
+
+    return (
+        <div className='flex ml-4 mt-6 flex-wrap'>
+            <div id='first-row' className='flex w-full'>
+                <p id='day' className='text-6xl mr-3'>{days[Today.getDay()-1]}</p>
+                <p id='dayNum' className='text-6xl mr-3'>{Today.getDate()}</p>
+                <p id='monthName' className='text-6xl mr-3'>{months[Today.getMonth()]}</p>
+            </div>
+            <div id='second-row'>
+                <p id='unusualHoliday' className='text-xl'>{unusualHoliday}</p>
+            </div>
+        </div>
+    )
+}
+
+type ToDoProp = {
+    Time : string,
+    Task : string,
+}
+const TodoEntity = ({Time, Task} : ToDoProp) => {
+    return(
+        <div className='w-10/12 shadow-xl pl-3.5 pb-3 pt-3 rounded-3xl border-2 border-cyan-700 hover:scale-110 transition mb-5'>
+            <div id='taskInfoContainer' className='w-4/5 float-left mt-auto mb-auto'>
+                <p>{Time}</p>
+                <p className='text-2xl'>{Task}</p>
+            </div>
+            <div id='completeButton-container' className='w-1/5 float-left h-full'>
+                <div id='completeButton' className='rounded-full flex justify-center items-center text-xl h-full'><DoneIcon className='fill-cyan-700 h-12 hover:fill-cyan-500'/></div>
+            </div>
+        </div>
+    );
+}
+
+const todos = [
+    {time: "16:00", name: "Silłownia"},
+    {time: "20:00", name: "Spacer z psem"}
+];
+
+const TasksForToday = () => {
+    return(
+        <div className='mt-8'>
+            <p className='ml-4 text-4xl'>Zadania na dzisiaj</p>
+            <div id='toosList' className='flex w-full justify-center mt-8 flex-wrap'>
+                {todos.map(todo => {
+                    return TodoEntity({Time: todo.time, Task: todo.name});
+                })}
+            </div>
+        </div>
+    );
+}
+
+const AddTaskButton = () => {
+    return (
+        <div className='absolute bottom-5 right-5 rounded-full w-20 h-20 bg-cyan-700 hover:bg-cyan-500 flex justify-center items-center'><PlusIcon className='fill-white h-1/2'/></div>
+    );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+      <TodayInfo/>
+      <TasksForToday/>
+      <AddTaskButton/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

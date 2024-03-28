f8d3be4d9004b20968cb792e2f6f165b419bbf0b
jakubw <jakub.wierzchowski4@gmail.com>
2024-03-28 20:02:59 +0100

    adding tasks is working ; TODO: deleting tasks

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // eslint-disable-next-line import/no-webpack-loader-syntax
@@ -12,138 +12,150 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const TodayInfo = () => {
+const PlannerApp = () => {
+    const TodayInfo = () => {
 
-    const days = ["Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Nie"]
-    const months = ["sty.", "lut.", "mar.", "kwi.", "maj", "cze.", "lip.", "sie.", "wrz.", "paź.", "lis.", "gru."];
+        const days = ["Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Nie"]
+        const months = ["sty.", "lut.", "mar.", "kwi.", "maj", "cze.", "lip.", "sie.", "wrz.", "paź.", "lis.", "gru."];
 
-    const Today : Date = new Date();
+        const Today : Date = new Date();
 
-    const [unusualHoliday, setUnusualHoliday] = useState<string>("");
+        const [unusualHoliday, setUnusualHoliday] = useState<string>("");
 
-    useEffect(() => {
-        const fetchUnusualHoliday = async () => {
-            const url = `https://pniedzwiedzinski.github.io/kalendarz-swiat-nietypowych/${Today.getMonth()+1}/${Today.getDate()}.json`;
-            try {
-                const data = await fetch(url);
-                if(!data.ok){
-                    throw new Error("error while fetching data")
+        useEffect(() => {
+            const fetchUnusualHoliday = async () => {
+                const url = `https://pniedzwiedzinski.github.io/kalendarz-swiat-nietypowych/${Today.getMonth()+1}/${Today.getDate()}.json`;
+                try {
+                    const data = await fetch(url);
+                    if(!data.ok){
+                        throw new Error("error while fetching data")
+                    }
+
+                    const unusualHoliday = await data.json();
+                    setUnusualHoliday(unusualHoliday[Math.floor(Math.random()*unusualHoliday.length)].name);
+                }catch(error){
+                    console.log(error);
                 }
-
-                const unusualHoliday = await data.json();
-                setUnusualHoliday(unusualHoliday[Math.floor(Math.random()*unusualHoliday.length)].name);
-            }catch(error){
-                console.log(error);
             }
-        }
 
-        fetchUnusualHoliday();
-    }, [Today])
+            fetchUnusualHoliday();
+        }, [Today])
 
-    return (
-        <div className='flex ml-4 mt-6 flex-wrap'>
-            <div id='first-row' className='flex w-full'>
-                <p id='day' className='text-6xl mr-3'>{days[Today.getDay()-1]}</p>
-                <p id='dayNum' className='text-6xl mr-3'>{Today.getDate()}</p>
-                <p id='monthName' className='text-6xl mr-3'>{months[Today.getMonth()]}</p>
-            </div>
-            <div id='second-row'>
-                <p id='unusualHoliday' className='text-xl'>{unusualHoliday}</p>
-            </div>
-        </div>
-    )
-}
-
-type ToDoProp = {
-    Time : string,
-    Task : string,
-}
-const TodoEntity = ({Time, Task} : ToDoProp) => {
-    return(
-        <div className='w-10/12 shadow-xl pl-3.5 pb-3 pt-3 rounded-3xl border-2 border-cyan-700 hover:scale-110 transition mb-5'>
-            <div id='taskInfoContainer' className='w-4/5 float-left mt-auto mb-auto'>
-                <p>{Time}</p>
-                <p className='text-2xl'>{Task}</p>
-            </div>
-            <div id='completeButton-container' className='w-1/5 float-left h-full flex items-center justify-end'>
-                <DoneIcon className='fill-cyan-700 h-12 hover:fill-cyan-500 w-12 mr-5 cursor-pointer transition'/>
-            </div>
-        </div>
-    );
-}
-
-type Todo = {
-    time: string,
-    name: string,
-};
-
-export const Todos = React.createContext<Todo[]>([{time: "16:00", name: "Silłownia"},
-                                                                                  {time: "20:00", name: "Spacer z psem"}])
-
-const TasksForToday = () => {
-
-    const todos = useContext(Todos);
-
-    return (
-        <div className='mt-8'>
-            <p className='ml-4 text-4xl'>Zadania na dzisiaj</p>
-            <div id='toosList' className='flex w-full justify-center mt-8 flex-wrap'>
-                {todos.map(todo => {
-                    return TodoEntity({Time: todo.time, Task: todo.name});
-                })}
-            </div>
-        </div>
-    );
-}
-
-const TaskInput = () => {
-
-    const [taskInputVisible, setTaskInputVisible] = useState<boolean>(true);
-
-    const disableTaskInput = (e : React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        setTaskInputVisible(false);
-    }
-
-    const enableTaskInput = (e : React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        setTaskInputVisible(true);
-    }
-
-    const AddTaskButton = () => {
         return (
-            <div onClick={enableTaskInput} className='absolute bottom-5 right-5 rounded-full w-20 h-20 bg-cyan-700 hover:bg-cyan-500 flex justify-center items-center cursor-pointer transition'><PlusIcon className='fill-white h-1/2'/></div>
+            <div className='flex ml-4 mt-6 flex-wrap'>
+                <div id='first-row' className='flex w-full'>
+                    <p id='day' className='text-6xl mr-3'>{days[Today.getDay()-1]}</p>
+                    <p id='dayNum' className='text-6xl mr-3'>{Today.getDate()}</p>
+                    <p id='monthName' className='text-6xl mr-3'>{months[Today.getMonth()]}</p>
+                </div>
+                <div id='second-row'>
+                    <p id='unusualHoliday' className='text-xl'>{unusualHoliday}</p>
+                </div>
+            </div>
+        )
+    }
+
+    const TodoEntity = ({time, name} : Todo, key : number) => {
+        return(
+            <div key={key} className='w-10/12 shadow-xl pl-3.5 pb-3 pt-3 rounded-3xl border-2 border-cyan-700 hover:scale-110 transition mb-5'>
+                <div id='taskInfoContainer' className='w-4/5 float-left mt-auto mb-auto'>
+                    <p>{time}</p>
+                    <p className='text-2xl'>{name}</p>
+                </div>
+                <div id='completeButton-container' className='w-1/5 float-left h-full flex items-center justify-end'>
+                    <DoneIcon className='fill-cyan-700 h-12 hover:fill-cyan-500 w-12 mr-5 cursor-pointer transition'/>
+                </div>
+            </div>
         );
     }
 
-    return(
-        <div>
-            <AddTaskButton/>
-            <div className={taskInputVisible ? 'absolute top-0 right-0 w-80 h-screen z-10 bg-white shadow-2xl border-l-2 border-cyan-700 transition-all' : 'absolute top-0 w-80 h-screen z-10 bg-white shadow-2xl border-l-2 border-cyan-700 -right-96 hidden transition-all'}>
-                <div className='flex items-center justify-stretch'>
-                    <p className='text-4xl mt-2 ml-2 float-left'>Dodaj zadanie</p>
-                    <div onClick={disableTaskInput} className='w-12 h-12 rounded-full bg-cyan-700 float-left mt-2 ml-auto mr-auto flex justify-center items-center hover:bg-cyan-500 transition cursor-pointer'>
-                        <CloseIcon className='fill-white w-3/4 h-3/4'/>
-                    </div>
+    type Todo = {
+        time: string,
+        name: string,
+    };
+
+     const [todos, setTodos] = useState<Todo[]>([{time: "16:00", name: "Silłownia"},
+        {time: "20:00", name: "Spacer z psem"}])
+
+    const TasksForToday = () => {
+
+        return (
+            <div className='mt-8'>
+                <p className='ml-4 text-4xl'>Zadania na dzisiaj</p>
+                <div id='toosList' className='flex w-full justify-center mt-8 flex-wrap'>
+                    {todos.map((todo, key) => {
+                        return TodoEntity({time: todo.time, name: todo.name}, key);
+                    })}
                 </div>
-                <div className='w-full'>
-                    <div className='flex w-full justify-center flex-wrap mt-5'>
-                        <p className='w-full pl-10'>Nazwa zadania</p>
-                        <input type='text' className='border-2 border-cyan-700 rounded-xl w-3/4 text-xl'></input></div>
-                    <div className='flex w-full justify-center flex-wrap mt-2'>
-                        <p className='w-full pl-10'>Godzina</p>
-                        <input type='time' className='border-2 border-cyan-700 rounded-xl w-3/4 text-xl'></input>
+            </div>
+        );
+    }
+
+    const TaskInput = () => {
+
+        const [taskInputVisible, setTaskInputVisible] = useState<boolean>(false);
+
+        const disableTaskInput = (e : React.MouseEvent<HTMLDivElement>) => {
+            e.preventDefault();
+            setTaskInputVisible(false);
+        }
+
+        const enableTaskInput = (e : React.MouseEvent<HTMLDivElement>) => {
+            e.preventDefault();
+            setTaskInputVisible(true);
+        }
+
+        const AddTaskButton = () => {
+            return (
+                <div onClick={enableTaskInput} className='absolute bottom-5 right-5 rounded-full w-20 h-20 bg-cyan-700 hover:bg-cyan-500 flex justify-center items-center cursor-pointer transition'><PlusIcon className='fill-white h-1/2'/></div>
+            );
+        }
+
+        const newTodo = {time: "", name: ""};
+
+        const handleNameInput = (e : React.FormEvent<HTMLInputElement>) => {
+            e.preventDefault();
+            newTodo.name = (e.target as HTMLInputElement).value;
+        }
+
+        const handleTimeInput = (e : React.FormEvent<HTMLInputElement>) => {
+            e.preventDefault();
+            newTodo.time = (e.target as HTMLInputElement).value;
+        }
+
+        const addTodo = () => {
+            if(newTodo.time !== "" && newTodo.name !== ""){
+                setTodos(prevTodos => [...prevTodos, newTodo]);
+            }
+        }
+
+        return(
+            <div>
+                <AddTaskButton/>
+                <div className={taskInputVisible ? 'absolute top-0 right-0 w-80 h-screen z-10 bg-white shadow-2xl border-l-2 border-cyan-700 transition-all' : 'absolute top-0 w-80 h-screen z-10 bg-white shadow-2xl border-l-2 border-cyan-700 -right-96 hidden transition-all'}>
+                    <div className='flex items-center justify-stretch'>
+                        <p className='text-4xl mt-2 ml-2 float-left'>Dodaj zadanie</p>
+                        <div onClick={disableTaskInput} className='w-12 h-12 rounded-full bg-cyan-700 float-left mt-2 ml-auto mr-auto flex justify-center items-center hover:bg-cyan-500 transition cursor-pointer'>
+                            <CloseIcon className='fill-white w-3/4 h-3/4'/>
+                        </div>
                     </div>
-                    <div className='w-full flex justify-center mt-5'>
-                        <div className='w-1/2 flex justify-center items-center bg-cyan-700 text-white pt-3 pb-3 rounded-full text-xl hover:bg-cyan-500 cursor-pointer transition'>Dodaj zadanie</div>
+                    <div className='w-full'>
+                        <div className='flex w-full justify-center flex-wrap mt-5'>
+                            <p className='w-full pl-10'>Nazwa zadania</p>
+                            <input type='text' className='border-2 border-cyan-700 rounded-xl w-3/4 text-xl' onInput={handleNameInput}></input></div>
+                        <div className='flex w-full justify-center flex-wrap mt-2'>
+                            <p className='w-full pl-10'>Godzina</p>
+                            <input type='time' className='border-2 border-cyan-700 rounded-xl w-3/4 text-xl' onInput={handleTimeInput}></input>
+                        </div>
+                        <div className='w-full flex justify-center mt-5'>
+                            <div onClick={addTodo} className='w-1/2 flex justify-center items-center bg-cyan-700 text-white pt-3 pb-3 rounded-full text-xl hover:bg-cyan-500 cursor-pointer transition'>Dodaj zadanie</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-const PlannerApp = () => {
     return(
         <React.StrictMode>
             <TodayInfo/>
